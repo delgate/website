88c45487ae92997e7270909d0a9fe71bd1a0b23c
d3vv3 <jaimecondel@gmail.com>
2023-11-07 12:22:54 +0100

    feat(website): added support for umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://analytics.sgn.space/script.js" data-website-id="6dedaab7-8269-4b2a-a76a-61f6ff2e3315"></script>
+      </head>
       <body className="dark:bg-blue-950 bg-gradient-to-tr from-slate-100 to-blue-200 dark:from-slate-900 dark:to-blue-950">{children}</body>
     </html>
   )
