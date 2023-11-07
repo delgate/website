28a7d32334094ca3d3c2a5b6f7985cc9df4238eb
d3vv3 <jaimecondel@gmail.com>
2023-11-07 11:37:02 +0100

    feat(style): initial website style

---
+++ src/app/components/Message.js
@@ -0,0 +1,18 @@
+export default function Message({ message, isClient }) {
+
+    return (
+        <div className={`
+            w-full flex flex-row
+            ${isClient ? "justify-end" : "justify-start"}
+            my-2
+            ${isClient ? "pl-8" : "pr-8"}
+        `}>
+            <div className={`
+                p-4 rounded-xl max-w-xs min-w-fit drop-shadow-md
+                ${isClient ? "bg-green-100" : "bg-gray-100"}
+            `}>
+                {message}
+            </div>
+        </div>
+    )
+}
